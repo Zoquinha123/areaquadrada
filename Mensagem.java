7418e32853d2fc4b72e4d820ff37bf6f20f161a0
Zoquinha123 <gilson.morais@fatec.sp.gov.br>
2022-02-21 19:19:39 +0000

    Gilson tem

--- Mensagem.java
+++ Mensagem.java
@@ -1,6 +1,7 @@
 public class Mensagem{
     public static void main(String[] ags){
-        System.out.println("Fatec Pompeia");
+        int idade = 38;
+        System.out.println("Gilson tem " + idade + "+ 12 anos");
 
 
         
